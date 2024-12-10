47277bbaa394cc621d11c691f89288843438f7be
Talko Denis <107505040+TalkoDenis@users.noreply.github.com>
2024-12-10 17:29:38 +0100

    Update year_scripts.js

--- assets/ts/year_scripts.js
+++ assets/ts/year_scripts.js
@@ -1,1 +1,10 @@
+// Получаем текущий год
+const year: number = new Date().getFullYear();
 
+// Получаем элемент с id 'year' и проверяем его тип
+const yearElement: HTMLElement | null = document.getElementById('year');
+
+// Проверяем, существует ли элемент с таким id
+if (yearElement) {
+    yearElement.textContent = year.toString(); // Устанавливаем текстовое содержимое
+}
