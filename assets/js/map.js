fbf162cb4717af59d38f275f1cbcafb72b856f71
TalkoDA <azzi2710@gmail.com>
2025-08-31 16:37:33 +0200

    One more time fixed map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,31 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const map = L.map("map").setView([20, 0], 2);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        attribution: "© OpenStreetMap contributors"
-    }).addTo(map);
+    const mapContainer = document.getElementById("map");
+    if (!mapContainer._leaflet_id) {
 
-    fetch("{{ '/assets/data/points.json' | relative_url }}")
-        .then(res => res.json())
-        .then(points => {
-            points.forEach(p => {
-                const iconUrl = p.type === "city"
-                    ? "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png"
-                    : "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png";
+        const map = L.map("map").setView([20, 0], 2);
 
-                L.marker([p.lat, p.lng], {
-                    icon: L.icon({
-                        iconUrl,
-                        shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
-                        iconSize: [25, 41],
-                        iconAnchor: [12, 41],
-                        popupAnchor: [1, -34],
-                        shadowSize: [41, 41]
-                    })
-                }).addTo(map)
-                    .bindPopup(`<b>${p.name}</b>`);
-            });
-        })
-        .catch(err => console.error("Cannot load JSON:", err));
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution: "© OpenStreetMap contributors"
+        }).addTo(map);
+
+        fetch("{{ '/assets/data/points.json' | relative_url }}")
+            .then(res => res.json())
+            .then(points => {
+                points.forEach(p => {
+                    const iconUrl = p.type === "city"
+                        ? "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png"
+                        : "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png";
+
+                    L.marker([p.lat, p.lng], {
+                        icon: L.icon({
+                            iconUrl,
+                            shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+                            iconSize: [25, 41],
+                            iconAnchor: [12, 41],
+                            popupAnchor: [1, -34],
+                            shadowSize: [41, 41]
+                        })
+                    }).addTo(map)
+                        .bindPopup(`<b>${p.name}</b>`);
+                });
+            })
+            .catch(err => console.error("Cannot load JSON:", err));
+    }
+
 });
--- map.md
+++ map.md
@@ -8,14 +8,8 @@ permalink: /map/
 
 <div id="map"></div>
 
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-  integrity="sha512-p2cE6YJ4Z1rU1UIF1OtSWT8gJtIhXCTITVEWilQoNqT9XyM54q9WlH0I1GtIsfRSAxEPPYUajF2iGaI2B1rZzw=="
-  crossorigin=""/>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-  integrity="sha512-vMb2pTnQmFln1zp0eOipSA3w8EMXwqyI51Onx5O20nQ2YSmWvd9FTJMcjsPbkLAk7fNKfK0jD9Xo7hXgzMJ3ew=="
-  crossorigin=""></script>
-
-<script src="{{ '/assets/js/map.js' | relative_url }}"></script>
 <link rel="stylesheet" href="{{ '/assets/css/map.css' | relative_url }}">
-
+<script src="{{ '/assets/js/map.js' | relative_url }}"></script>
